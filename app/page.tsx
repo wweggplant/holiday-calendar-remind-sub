02b94c1965ae23cb8650c8e792e59b8dc6077441
wweggplant <weiainijiujiu@126.com>
2024-04-16 11:25:44 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return  (
+    <>
+      <nav className="w-full bg-white shadow">
+        <div className="container mx-auto px-4 py-4 flex justify-end items-center">
+          <Link href="https://github.com/your-username/your-repository" passHref className="text-gray-500 hover:text-gray-600" aria-label="GitHub Project">
+            <Image src="/github-mark.svg" alt="GitHub Logo" width={32} height={32} />
+          </Link>
+        </div>
+      </nav>
+      <div className="pt-20 flex flex-col items-center justify-center bg-blue-50 px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-700 mb-2">
+            火车票预约 | 一“键”掌握
+          </h1>
+          <p className="mb-4 text-sm sm:text-base text-gray-500">
+            导入日历，提醒节假日前后和调休当天预订火车票
+          </p>
+          <Link href='/bookTrain.ics' passHref className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-150 ease-in-out">
+            点击导入日历
+          </Link>
+          <div className="mt-10 relative w-full max-w-md h-auto">
+            <Image alt="海报图" layout='responsive' width={800} height={800} src={'/post.webp'} priority />
+          </div>
+        </div>
+      </div>
+    </>
+    
+  );
+}
---
+++ script/generate_book_train_ics.py
@@ -0,0 +1,95 @@
+import os
+import requests
+import datetime as dt
+import logging
+from icalendar import Calendar, Event
+import pytz
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Constant for the number of days before an event to create a reminder
+BOOKING_DAYS = 14
+DEFAULT_DIR = "../public/"
+# Helper function to fetch holidays data
+def fetch_holiday_booking_dates(year):
+    url = 'https://cdn.jsdelivr.net/gh/lanceliao/china-holiday-calender/holidayAPI.json'
+    response = requests.get(url)
+    data = response.json()
+    
+    booking_dates = set()
+    
+    for holiday in data['Years'].get(str(year), []):
+        start_date = dt.datetime.strptime(holiday['StartDate'], '%Y-%m-%d').date()
+        end_date = dt.datetime.strptime(holiday['EndDate'], '%Y-%m-%d').date()
+        booking_dates.update([start_date - dt.timedelta(days=1), end_date + dt.timedelta(days=1)])
+    
+    return booking_dates
+
+# Helper function to fetch compensatory work dates
+def fetch_compensatory_dates(year):
+    response = requests.get('https://cdn.jsdelivr.net/gh/lanceliao/china-holiday-calender/holidayCal-CO.ics')
+    calendar = Calendar.from_ical(response.text)
+    comp_dates = []
+
+    for component in calendar.walk('vevent'):
+        start = component.get('dtstart').dt.date()
+        if start.year == year:
+            comp_dates.append(start)
+
+    return comp_dates
+
+# Combine and sort holiday and compensatory dates along with regular weekday dates
+def compile_and_sort_dates(year):
+    holidays = fetch_holiday_booking_dates(year)
+    compensatory = fetch_compensatory_dates(year)
+    all_dates = set(holidays) | set(compensatory)
+    return sorted(all_dates)
+
+# Calculate reminder dates based on booking rules
+def calculate_reminder_dates(sorted_dates):
+    return [{'reminder_date': date - dt.timedelta(days=BOOKING_DAYS), 'book_date': date} for date in sorted_dates]
+
+# Generate an ICS file from a list of reminder and book dates
+def create_ics_file(reminders, filename="bookTrain.ics"):
+    cal = Calendar()
+    cal.add('prodid', '-//Custom Calendar//mxm.dk//')
+    cal.add('version', '2.0')
+    
+    for reminder in reminders:
+        event = Event()
+        event.add('summary', f'火车票提醒 - 订票日期: {reminder["book_date"].isoformat()}')
+        event.add('dtstart', dt.datetime.combine(reminder['reminder_date'], dt.time(11, 55, 0), tzinfo=pytz.utc))
+        event.add('dtend', dt.datetime.combine(reminder['reminder_date'], dt.time(16, 10, 0), tzinfo=pytz.utc))
+        event.add('dtstamp', dt.datetime.now(pytz.utc))
+        event['uid'] = f'{reminder["reminder_date"].isoformat()}-weiainijiujiu@126.com'
+        cal.add_component(event)
+
+    with open(filename, 'wb') as f:
+        f.write(cal.to_ical())
+    return filename
+
+# Main function to run as a GitHub Actions job
+def main():
+    year = dt.datetime.now().year
+    sorted_dates = compile_and_sort_dates(year)
+    reminder_dates = calculate_reminder_dates(sorted_dates)
+    
+    # Ensure the directory exists
+    if not os.path.exists(DEFAULT_DIR):
+        os.makedirs(DEFAULT_DIR)
+        logging.info(f"Created directory {DEFAULT_DIR}")
+    
+    base_filename = "bookTrain.ics"
+    full_path = os.path.join(DEFAULT_DIR, base_filename)
+    old_filename = os.path.join(DEFAULT_DIR, f"bookTrain-{year - 1}.ics")
+    
+    if os.path.exists(full_path):
+        os.rename(full_path, old_filename)
+        logging.info(f"Renamed old file to {old_filename}")
+    
+    ics_file = create_ics_file(reminder_dates, filename=full_path)
+    logging.info(f"ICS file created: {ics_file}")
+
+if __name__ == "__main__":
+    main()
