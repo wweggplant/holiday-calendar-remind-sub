c356c25102c9f7bce7ce26ed4e2b833577de58fd
wweggplant <weiainijiujiu@126.com>
2024-04-16 11:46:07 +0800

    Update GitHub repository link and navbar styling in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
-  return  (
+  return (
     <>
-      <nav className="w-full bg-white shadow">
-        <div className="container mx-auto px-4 py-4 flex justify-end items-center">
-          <Link href="https://github.com/wweggplant/holiday-calendar-remind-sub" passHref className="text-gray-500 hover:text-gray-600" aria-label="GitHub Project">
+      <nav className="fixed top-0 z-10 w-full bg-white shadow">
+        <div className="container mx-auto px-4 py-2 flex justify-end items-center">
+          <Link href="https://github.com/wweggplant/holiday-calendar-remind-sub" aria-label="GitHub Project" className="text-gray-500 hover:text-gray-600">
             <Image src="/github-mark.svg" alt="GitHub Logo" width={32} height={32} />
           </Link>
         </div>
       </nav>
-      <div className="pt-20 flex flex-col items-center justify-center bg-blue-50 px-4 sm:px-6 lg:px-8">
+      <div className="pt-16 flex flex-col items-center justify-center bg-blue-50 min-h-screen px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-700 mb-2">
             火车票预约 | 一“键”掌握
@@ -19,7 +19,7 @@ export default function Home() {
           <p className="mb-4 text-sm sm:text-base text-gray-500">
             导入日历，提醒节假日前后和调休当天预订火车票
           </p>
-          <Link href='/bookTrain.ics' passHref className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-150 ease-in-out">
+          <Link href='/bookTrain.ics' className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-150 ease-in-out">
             点击导入日历
           </Link>
           <div className="mt-10 relative w-full max-w-md h-auto">
@@ -28,6 +28,5 @@ export default function Home() {
         </div>
       </div>
     </>
-    
   );
 }
